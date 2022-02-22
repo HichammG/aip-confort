f089c3323c23179fb6096d0a5fc8bb05aa310d0e
Hicham GARMATE <hicham.garmate@gmail.com>
2022-02-22 18:53:25 +0100

    add material

--- static/romain/js/main.js
+++ static/romain/js/main.js
@@ -219,7 +219,7 @@
                     //logs
                     console.log('khabaj');
                     console.log(logiciels);
-                    console.log(Nbpc);
+                    console.log(Nbplaces);
                     console.log(Projecteur);
                     console.log(Nbpc);
                     var div = '<div style="text-align: center;" class="card"><div style="text-align: center;" class="card-header" id="Salle_{ID}_Heading"><h2 class="mb-0 text-center" style="text-align: center; width: 100%;"><button style="text-align: center;" class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#Salle_{ID}_Body" aria-expanded="false" aria-controls="Salle_{ID}_Body">{content} à {Note}/10 <div id="salle_rater_{ID}"></div></button></h2></div><div id="Salle_{ID}_Body" class="collapse" aria-labelledby="Salle_{ID}_Heading" data-parent="#DonneesClassementDiv"><div class="card-body"><div><strong>Temperature:</strong><span>{TEMPERATURE} °C</span></div><div><strong>Acoustique:</strong><span>{Acoustique} dB</span></div><div><strong>Humidite:</strong><span>{Humidite} %</span></div><div><strong>Luminosité:</strong><span>{Lum} lux</span></div><div><strong>CO2:</strong><span>{CO2} ppm</span></div><div><strong>Cette salle dispose de :</strong><span></span></div><div><strong>Logiciels :</strong><span>{Logiciels}</span></div><div><strong>Nombre de places :</strong><span>{Nbplaces}</span></div><div><strong>Nombre de PCs :</strong><span>{Nbpc}</span></div><div><strong>Projecteur :</strong><span>{Projecteur}</span></div></div></div></div>';
